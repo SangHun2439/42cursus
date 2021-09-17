4629b65ddcfc199bc893fd643a1d42ff80fa43a5
전상훈 <jeonsanghun@jeonsanghun-ui-MacBookAir.local>
2021-09-17 10:41:51 +0900

    correct leaks checker

--- push_swap/checker_bonus.c
+++ push_swap/checker_bonus.c
@@ -6,20 +6,24 @@
 /*   By: sangjeon <sangjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 17:19:56 by sangjeon          #+#    #+#             */
-/*   Updated: 2021/09/07 00:58:46 by sangjeon         ###   ########.fr       */
+/*   Updated: 2021/09/17 10:24:45 by sangjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
 
-int	ko(void)
+int	ko(t_deque **p_a_deq, t_deque **p_b_deq)
 {
+	ft_deqclear(p_a_deq, del);
+	ft_deqclear(p_b_deq, del);
 	ft_putstr_fd("KO\n", 1);
 	return (1);
 }
 
-int	ok(void)
+int	ok(t_deque **p_a_deq, t_deque **p_b_deq)
 {
+	ft_deqclear(p_a_deq, del);
+	ft_deqclear(p_b_deq, del);
 	ft_putstr_fd("OK\n", 1);
 	return (1);
 }
@@ -30,7 +34,7 @@ int	sort_check(t_deque *a_deq, t_deque *b_deq)
 	t_node	*head;
 
 	if (!ft_deq_isempty(b_deq))
-		return (ko());
+		return (ko(&a_deq, &b_deq));
 	while (!ft_deq_isempty(a_deq))
 	{
 		val = (int *)ft_deqpop_front(a_deq);
@@ -38,14 +42,12 @@ int	sort_check(t_deque *a_deq, t_deque *b_deq)
 		while (head)
 		{
 			if (*val > *(int *)head->content)
-				return (ko());
+				return (ko(&a_deq, &b_deq));
 			head = head->next;
 		}
 		del(val);
 	}
-	ft_deqclear(&a_deq, del);
-	ft_deqclear(&b_deq, del);
-	return (ok());
+	return (ok(&a_deq, &b_deq));
 }
 
 int	intersection(t_deque *a_deq, t_deque *b_deq, char *str)
@@ -90,8 +92,11 @@ void	do_sort(t_deque *a_deq, t_deque *b_deq, int *err)
 			*err = 1;
 			return ;
 		}
+		free(str);
 		errcheck = get_next_line(0, &str);
 	}
 	if (errcheck < 0)
 		*err = 1;
+	else
+		free(str);
 }
