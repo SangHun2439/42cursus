6a129cde97a7f4a8705945d37def2ab8a6275eaf
SangHun2439 <privacyab@gmail.com>
2022-03-06 01:53:12 +0900

    modify makefile

--- cpp/cpp08/ex02/main.cpp
+++ cpp/cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sangjeon <sangjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 23:57:55 by sangjeon          #+#    #+#             */
-/*   Updated: 2022/03/02 01:25:44 by sangjeon         ###   ########.fr       */
+/*   Updated: 2022/03/06 01:34:15 by sangjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
+
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
